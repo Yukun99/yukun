83e0f8985cb2c8b5aeb1017ca522dea123482f70
Xu, Yukun <yukun.xu@juliusbaer.com>
2026-06-25 11:27:12 +0800

    feat: Make Page Responsive to Device Layout Changes - make page reload when device type changes - fix formatting and layout for mobile site

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,16 +1,28 @@
 import Navigation from '@/features/navigation/navigation';
 import { useRoutes } from 'react-router-dom';
 import routes from '@/app/routes';
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import bg from '@/assets/bg.jpg';
 import Box from '@mui/material/Box';
 import { OPACITY } from '@/app/palette';
 import useDocumentTitle from '@/common/hooks/use-document-title';
+import useTheme from '@mui/system/useTheme';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const AppRoutes = () => useRoutes(routes);
 
 const App = () => {
   useDocumentTitle();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const [wasMobile, setWasMobile] = useState<boolean>(isMobile);
+
+  useEffect(() => {
+    if (isMobile !== wasMobile) {
+      setWasMobile(isMobile);
+      window.location.reload();
+    }
+  }, [isMobile]);
 
   return (
     <Box
--- apps/frontend/src/pages/home/home.tsx
+++ apps/frontend/src/pages/home/home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
   return (
     <Box sx={{ gap: '8px', marginX: '16px', width: 'calc(100vw - 32px)' }}>
       <Box sx={{ display: 'flex', flexDirection: isMobileOnly ? 'column-reverse' : 'row' }}>
-        <HomeSection>
+        <HomeSection width={isMobileOnly ? undefined : '80%'}>
           <Typography variant='h3' sx={{ fontWeight: 'bold' }}>
             {t('home.intro.title')}
           </Typography>
@@ -31,38 +31,38 @@ const Home = () => {
             {t('home.intro.4')}
           </Typography>
         </HomeSection>
-        <HomeSection>
-          <Box sx={{ width: '25vw' }}>
-            <Typography variant='h3' sx={{ fontWeight: 'bold' }}>
-              {t('home.details.title')}
-            </Typography>
-            {t('home.details.name', { name: t('common.name') })}
-            <br />
-            <br />
-            {t('home.details.gender', { gender: t('common.gender') })}
-            <br />
-            <br />
-            <Trans
-              i18nKey='home.details.email'
-              values={{ email }}
-              components={{ email: <Link href={`mailto:${email}`} /> }}
-            />
-            <br />
-            <br />
-            {t('home.details.mobile', { mobile: t('common.mobile') })}
-            <br />
-            <br />
-            <Trans
-              i18nKey='home.details.linkedin'
-              values={{ linkedin }}
-              components={{
-                linkedin: <Link href={linkedin} target='_blank' rel='noopener noreferrer' />,
-              }}
-            />
-          </Box>
+        <HomeSection width={isMobileOnly ? undefined : '40%'}>
+          <Typography variant='h3' sx={{ fontWeight: 'bold' }}>
+            {t('home.details.title')}
+          </Typography>
+          {t('home.details.name', { name: t('common.name') })}
+          <br />
+          <br />
+          {t('home.details.gender', { gender: t('common.gender') })}
+          <br />
+          <br />
+          <Trans
+            i18nKey='home.details.email'
+            values={{ email }}
+            components={{
+              email: <Link href={`mailto:${email}`} target='_blank' rel='noopener noreferrer' />,
+            }}
+          />
+          {!isMobileOnly && <br />}
+          <br />
+          {t('home.details.mobile', { mobile: t('common.mobile') })}
+          <br />
+          <br />
+          <Trans
+            i18nKey='home.details.linkedin'
+            values={{ linkedin }}
+            components={{
+              linkedin: <Link href={linkedin} target='_blank' rel='noopener noreferrer' />,
+            }}
+          />
         </HomeSection>
         <HomeSection style={{ justifyContent: 'center' }}>
-          <Box component='img' src={me} sx={{ width: '15vw', height: 'auto' }} />
+          <Box component='img' src={me} sx={{ height: 'auto', width: '100%' }} />
         </HomeSection>
       </Box>
       <HomeSection>
