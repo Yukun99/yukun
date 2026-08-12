8011129a5c3d6796f1516189e97f5fefcef4b92e
Xu, Yukun <yukun.xu@juliusbaer.com>
2026-08-12 22:44:27 +0800

    fix: Reduce Circle Count for Mobile Catalog Component for Week 5

--- apps/frontend/src/pages/catalog/components/week5.tsx
+++ apps/frontend/src/pages/catalog/components/week5.tsx
@@ -1,8 +1,10 @@
 import { OPACITY } from '@/app/palette';
 import FloatingCircles from '@/common/components/effects/floating-circles';
+import useIsMobile from '@/common/hooks/use-is-mobile';
 
 const Week5Example = () => {
-  return <FloatingCircles count={100} opacity={OPACITY[50]} />;
+  const isMobile = useIsMobile();
+  return <FloatingCircles count={isMobile ? 50 : 100} opacity={OPACITY[50]} />;
 };
 
 export default Week5Example;
